427806b6219648be2442a0d39aee9ee2b90682f6
Pooja S <pooja.sangitrao@smartdatainc.net>
2020-08-18 17:14:58 +0530

    angular codes

--- angularcrud/src/app/app.module.ts
+++ angularcrud/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule,Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CompaintListComponent } from './compaint-list/compaint-list.component';
+import { AddComplaintComponent } from './complaint/add-complaint/add-complaint.component';
 
+const appRoutes: Routes = [
+  { path: 'list', component:CompaintListComponent},
+  { path: 'add', component:AddComplaintComponent},
+  { path: '', redirectTo: '/list', pathMatch:'full'}
+];
 @NgModule({
   declarations: [
     AppComponent,
-    CompaintListComponent
+    CompaintListComponent,
+    AddComplaintComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
